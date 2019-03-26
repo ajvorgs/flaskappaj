3a6df7226af8c8b325a1d2602e13ec19d36b46d1
ajvorgs <ajajborbz@gmail.com>
2019-03-26 04:04:35 -0700

    Major changes, end of development

--- app.py
+++ app.py
@@ -159,6 +159,45 @@ class ArticleForm(Form):
     body = TextAreaField('Body', [validators.Length(min=30)])
 
 # Add Articles
+@app.route('/edit_article/<string:id>', methods=['GET', 'POST'])
+@is_logged_in
+def edit_article(id):
+    #create cursor
+    cur = mysql.connection.cursor()
+
+    # Get the article by id
+    result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+
+    article = cur.fetchone()
+
+    #get form
+    form = ArticleForm(request.form)
+
+    #Populate article form fields
+    form.title.data = article['title']
+    form.body.data = article['body']
+
+    if request.method == 'POST' and form.validate():
+        title = request.form['title']
+        body = request.form['body']
+
+        #Create cursor
+        cur = mysql.connection.cursor()
+
+        #execute
+        cur.execute("UPDATE articles SET title=%s,body=%s WHERE id=%s" ,(title,body,id))
+
+        # commit
+        mysql.connection.commit()
+
+        #Close
+        cur.close()
+        flash('Article Updated', 'success')
+        return redirect(url_for('dashboard'))
+
+    return render_template('edit_article.html', form=form)
+
+# Edit Articles
 @app.route('/add_article', methods=['GET', 'POST'])
 @is_logged_in
 def add_article():
@@ -183,6 +222,24 @@ def add_article():
 
     return render_template('add_article.html', form=form)
 
+#Delete Article
+@app.route('/delete_article/<string:id>' , methods =['POST'])
+@is_logged_in
+def delete_article(id):
+
+    #create cursor
+    cur = mysql.connection.cursor()
+
+    # Execute
+    cur.execute("DELETE FROM articles WHERE id=%s", [id])
+
+    # commit
+    mysql.connection.commit()
+
+    #Close
+    cur.close()
+    flash('Article Deleted', 'success')
+    return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
     app.secret_key = 'secret123'
